93f9c5cf1393b537c86489a158e42db01529b8d1
Nerdix <70015952+N3rdix@users.noreply.github.com>
2023-08-31 08:29:28 +0200

    Add files via upload

---
+++ images/code/const.h
@@ -0,0 +1,2 @@
+#define SENSOR 2 // define pint 2 for sensor
+#define ACTION 9 // define pin 9 as for ACTION
---
+++ images/code/setup.h
@@ -0,0 +1,11 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#include <Arduino.h>
+#include "const.h"
+#include <Servo.h>
+
+void setupSensor();
+void setupServo(Servo &servo);
+
+#endif
